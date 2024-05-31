cb344f003578cea5ca34712ea9584af7215898b6
市川恭佑 <github@mail.ebi-yade.com>
2024-05-31 18:15:38 +0900

    Return bool to fallback

--- common.go
+++ common.go
@@ -7,13 +7,13 @@ import (
 	"strings"
 )
 
-type StringFunc func(val string) string
+type StringFunc func(val string) (string, bool)
 
-type StringFuncWithError func(val string) (string, error)
+type StringFuncWithError func(val string) (string, bool, error)
 
-type StringFuncWithContext func(ctx context.Context, val string) string
+type StringFuncWithContext func(ctx context.Context, val string) (string, bool)
 
-type StringFuncWithContextError func(ctx context.Context, val string) (string, error)
+type StringFuncWithContextError func(ctx context.Context, val string) (string, bool, error)
 
 type Prefix interface {
 	Match(string) bool
@@ -70,11 +70,20 @@ func (m StringsMux) Execute(args ...string) (string, error) {
 			switch fn := fn.(type) {
 			case StringFunc:
 				slog.Debug(fmt.Sprintf("executing StringFunc for %s", arg))
-				return fn(suffix), nil
+				val, ok := fn(suffix)
+				if ok {
+					return val, nil
+				}
 
 			case StringFuncWithError:
 				slog.Debug(fmt.Sprintf("executing StringFuncWithError for %s", arg))
-				return fn(suffix)
+				val, ok, err := fn(suffix)
+				if err != nil {
+					return "", err
+				}
+				if ok {
+					return val, nil
+				}
 			}
 		}
 
@@ -116,4 +125,82 @@ func (m *AsyncStringsMux) Validate() error {
 	return nil
 }
 
-// TODO: Implement Execute method for AsyncStringsMux
+func (m *AsyncStringsMux) Execute(args ...string) (string, error) {
+
+	type result struct {
+		val string
+		ok  bool
+		err error
+	}
+	results := make([]chan result, 0, len(args))
+	for range args {
+		results = append(results, make(chan result, 1))
+	}
+
+	ctx, cancel := context.WithCancel(m.Ctx)
+	defer cancel()
+
+	// 非同期処理の発火または同期処理実行
+	// en: Fire asynchronous processing or execute synchronous processing
+	for index, arg := range args {
+		promise := results[index]
+		for prefix, fn := range m.Map {
+			if !prefix.Match(arg) {
+				continue
+			}
+			suffix := prefix.Strip(arg)
+
+			switch fn := fn.(type) {
+			case StringFunc:
+				slog.Debug(fmt.Sprintf("executing StringFunc for %s", arg))
+				val, ok := fn(suffix)
+				promise <- result{val: val, ok: ok, err: nil}
+				close(promise)
+
+			case StringFuncWithError:
+				slog.Debug(fmt.Sprintf("executing StringFuncWithError for %s", arg))
+				val, ok, err := fn(suffix)
+				promise <- result{val: val, ok: ok, err: err}
+				close(promise)
+
+			case StringFuncWithContext:
+				slog.Debug(fmt.Sprintf("executing StringFuncWithContext for %s", arg))
+				go func() {
+					val, ok := fn(ctx, suffix)
+					promise <- result{val: val, ok: ok, err: nil}
+					close(promise)
+				}()
+
+			case StringFuncWithContextError:
+				slog.Debug(fmt.Sprintf("executing StringFuncWithContextError for %s", arg))
+				go func() {
+					val, ok, err := fn(ctx, suffix)
+					promise <- result{val: val, ok: ok, err: err}
+					close(promise)
+				}()
+
+			default:
+				if index == len(args)-1 {
+					promise <- result{val: arg, ok: true, err: nil}
+					close(promise)
+				} else {
+					return "", fmt.Errorf("no match found for %s", arg)
+				}
+			}
+		}
+	}
+
+	for _, promise := range results {
+		select {
+		case res := <-promise:
+			if res.err != nil {
+				return "", res.err
+			}
+			if res.ok {
+				return res.val, nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("no arguments provided")
+}
